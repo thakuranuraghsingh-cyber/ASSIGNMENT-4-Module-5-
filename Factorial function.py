8c1484152dc160e5cfd3c5d1d9a4fed66903b6b3
anuragsinghhcu-oss <anuragsinghhcu@gmail.com>
2025-10-10 03:35:16 +0530

    Deleted Factorial function.py and Math Module assignment.py

--- Factorial function.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    if n < 2:
-        return 1
-    else:
-        return n * (factorial(n-1))
-
-result = int(input("Enter the number: "))
-print("Factorial of",result ,"is:", factorial(result))
